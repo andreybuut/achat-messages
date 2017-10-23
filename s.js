6ec774a6ee2cb42fb42997c9a59b9306d290eb3c
Andre A. But <andrey.but@saritasa.com>
2017-10-23 12:27:43 +0600

    task: 33 pug tutoral complete, on rooms pug_tut

--- s.js
+++ s.js
@@ -1,7 +1,12 @@
-
-
-
-
-function test(){
-  console.log( "dsf" );
-}
+diff --git a/s.js b/s.js
+index cc4ca91..92ae1b5 100644
+--- a/s.js
++++ b/s.js
+@@ -1,7 +1,3 @@
+-
+-
+-
+-
+ function test(){
+   console.log( "dsf" );
+ }
